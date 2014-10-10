b13ddf71f696b8dfb5c505178dda6033007bc409
Bryance Oyang <bobianite@gmail.com>
2014-10-09 20:46:52 -0700

    commenting wat

--- boot.s
+++ boot.s
@@ -6,8 +6,13 @@
  */
 
 /*
+ * Note that memory segment registers (cs, ds, ss, es) refer to every 16
+ * bytes, i.e. 0x0123 in these registers refers to 0x1230 in memory
+ */
+
+/*
  * When the processor starts, it executes whatever is in a special
- * default memory address. This is mapped to the BIOS.
+ * default memory address. This is usually hardware-mapped to the BIOS.
  *
  * The first 512 bytes of a disk is the bootsector. The BIOS looks at
  * the last 2 bytes of the bootsector. These should be 0xaa55 to
@@ -18,19 +23,34 @@
  *
  * The following 2 commands will create the entire bootsector (assuming
  * gcc/linux):
- * as boot.s -c -o boot.o
- * ld --oformat binary --Ttext 0x7c00 --entry=blarg -o boot boot.o
+ *	as boot.s -c -o boot.o
+ *	ld --oformat binary --Ttext 0x7c00 --entry=blarg -o boot boot.o
  *
  * The ld is necessary to make the output the correct format.
- * "--oformat binary" specifies the output file should be pure binary,
- * i.e. (no ELF nonsense)
+ *
+ * "--oformat binary" specifies the output file should be pure binary
+ * machine code, i.e. (no ELF nonsense)
+ *
  * "--Ttext 0x7c00" specifies the absolute address for the .text section
- * to be 0x7c00 (because the bootsector is loaded there by the BIOS)
+ * to be 0x7c00 (because the bootsector is loaded there by the BIOS).
+ * Jump labels' addresses will all be adjusted to account for this
+ * offset. The linux kernel does not use this ld flag, and instead does
+ * the following:
+ *
+ *	ljmp	$0x07c0, $start2
+ * start2:
+ *	movw	%cs, %ax
+ *
+ * and then copies %ax to %ss, %ds, %es. ljmp has the format ljmp [cs
+ * register value], [ip register value]. Refer to arch/x86/boot/header.S
+ * in linux source code
+ *
  * "--entry=blarg" specifies that blarg is the entry point (like C
  * main())
  *
  * Now you have a 512 byte binary file containing the raw bootsector. Do
- * whatever you want with it (probably boot with it)
+ * whatever you want with it (probably boot it?) Suggestion:
+ *	dd if=boot of=/dev/u_know_the_drill
  *
  * See Makefile for how to ISO
  */
@@ -39,6 +59,7 @@
 .code16
 .text
 
+/* .global makes blarg visible to the linker ld */
 .global blarg
 blarg:
 	cli			# disable interrupts (clear bit)
@@ -60,13 +81,14 @@ done:
  */
 puts:
 	movb	(%di), %al
-	testb	%al, %al
+	testb	%al, %al	# assuming null terminated string
 	jz	puts_end
 
+	/* set up the interrupt args in the registers */
 	movb	$0x0e, %ah	# tty write interrupt
 	movb	$0x0a, %bl	# font color: sexy green, if possible
 	movb	$0x00, %bh	# page number
-	int	$0x10		# BIOS interrupt
+	int	$0x10		# activate BIOS video interrupt
 
 	inc	%di
 	jmp	puts
@@ -83,5 +105,5 @@ hehe:
  *
  * Either do ".org 510, 0" or ". = blarg + 510"
  */
-.org 510, 0
-.short 0xaa55
+.org 510, 0	# advance to 510, filling intermediate space with 0's
+.short 0xaa55	# write two bytes, 0x55 and 0xaa, in that order (little endian)
